003089eead3aa34b59bbff3fa4d169e500805fed
twel <registos@cardosodias.com>
2014-01-10 23:27:08 +0000

    1

---
+++ src/MyFirstGit.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: twel0n
+ * Date: 10-01-2014
+ * Time: 23:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyFirstGit {
+}
